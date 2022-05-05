b3c9e0528ca4d7a85c6bf2085348b6d659a53740
wendy.li <wendy.li@wanin.tw>
2022-05-06 00:33:26 +0800

    r

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    children: [
-      {
-        path: '/',
-        name: 'Wall',
-        component: () => import('../views/Page/Wall.vue'),
-      },
-      // 追蹤名單
-      {
-        path: '/user/:id/following',
-        name: 'Follow',
-        component: () => import('../views/Page/Following.vue'),
-      },
-      // 點讚的文章
-      {
-        path: '/user/:id/likes',
-        name: 'Likes',
-        component: () => import('../views/Page/Likes.vue'),
-      },
-      // 編輯個人資訊
-      {
-        path: '/user/:id/edit',
-        name: 'Edit',
-        component: () => import('../views/Page/Edit.vue'),
-      },
-      // 張貼文章
-      {
-        path: '/user/:id/posts',
-        name: 'Post',
-        component: () => import('../views/Page/Post.vue'),
-      },
-    ],
-  },
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: () => import('../views/Home.vue'),
+  //   children: [
+  //     {
+  //       path: '/',
+  //       name: 'Wall',
+  //       component: () => import('../views/Page/Wall.vue'),
+  //     },
+  //     // 追蹤名單
+  //     {
+  //       path: '/user/:id/following',
+  //       name: 'Follow',
+  //       component: () => import('../views/Page/Following.vue'),
+  //     },
+  //     // 點讚的文章
+  //     {
+  //       path: '/user/:id/likes',
+  //       name: 'Likes',
+  //       component: () => import('../views/Page/Likes.vue'),
+  //     },
+  //     // 編輯個人資訊
+  //     {
+  //       path: '/user/:id/edit',
+  //       name: 'Edit',
+  //       component: () => import('../views/Page/Edit.vue'),
+  //     },
+  //     // 張貼文章
+  //     {
+  //       path: '/user/:id/posts',
+  //       name: 'Post',
+  //       component: () => import('../views/Page/Post.vue'),
+  //     },
+  //   ],
+  // },
   {
     path: '/Login',
     name: 'Login',
